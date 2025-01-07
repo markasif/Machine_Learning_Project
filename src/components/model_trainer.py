d4abd0af631f898cee6b3c3d4ef6463ed00282e3
markasif <apasif243@gmail.com>
2025-01-07 16:09:12 +0530

    Hyper parameter Tuning

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -42,14 +42,41 @@ class ModelTrainer:
                 "Random Forest Regressor": RandomForestRegressor(),
                 "XGBRegressor": XGBRegressor(), 
                 "AdaBoost Regressor": AdaBoostRegressor()
-                }
-            model_report:dict= evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,models=models)
+             }
+            params = {
+                        "Decision Tree": {
+                        'criterion': ['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                        },
+                        "Random Forest Regressor": {
+                        'n_estimators': [8, 16, 32, 64, 128, 256]
+                        },
+                        "Gradient Boosting": {
+                        'learning_rate': [.1, .01, .05, .001],
+                        'subsample': [0.6, 0.7, 0.75, 0.8, 0.85, 0.9],
+                        'n_estimators': [8, 16, 32, 64, 128, 256]
+                        },
+                        "Linear Regression": {},
+                        "K-Neighbors Regressor": {  # Match the key here
+                        'n_neighbors': [5, 7, 9, 11]
+                        },
+                        "XGBRegressor": {
+                        'learning_rate': [.1, .01, .05, .001],
+                        'n_estimators': [8, 16, 32, 64, 128, 256]
+                        },
+                        "AdaBoost Regressor": {
+                        'learning_rate': [.1, .01, 0.5, .001],
+                        'n_estimators': [8, 16, 32, 64, 128, 256]
+    }
+}
+
+    
+            model_report:dict= evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,models=models,param=params)
 
             best_model_score = max(sorted(model_report.values()))
             best_model_name = list(model_report.keys())[
                 list(model_report.values()).index(best_model_score)
             ]
-
+ 
             best_model =models[best_model_name]
 
             if best_model_score <0.6:
